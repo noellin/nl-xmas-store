981fa925b6824ff62360662969846b0a5f06f8ee
Noel <jkuiop66@hotmail.com>
2018-11-27 11:24:13 +0800

    router修正

--- src/router.js
+++ src/router.js
@@ -1,49 +1,49 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Shop from '@/views/Shop';
-import AllItem from '@/views/AllItem';
-import SingleItem from '@/views/SingleItem';
-import Faq from '@/views/Faq';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Shop from "@/views/Shop";
+import AllItem from "@/views/AllItem";
+import SingleItem from "@/views/SingleItem";
+import Faq from "@/views/Faq";
 import CreateOrder from "@/views/CreateOrder";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home,
       children: [
-      {
-      	path: '',
-      	name: 'Shop',
-      	component: Shop,
-      	children: [
-      	{
-      		path: '',
-      		name: 'AllItem',
-      		component: AllItem
-      	},
-      	{
-      		path: 'product/:itemId',
-      		name: 'SingleItem',
-      		component: SingleItem
-      	},
-      	]
-      },
-      {
-      	path: 'faq',
-      	name: 'Faq',
-      	component: Faq
-      },
-      {
-      	path: 'createOrder',
-      	name: 'CreateOrder',
-      	component: CreateOrder
-      },
+        {
+          path: "",
+          name: "Shop",
+          component: Shop,
+          children: [
+            {
+              path: "",
+              name: "AllItem",
+              component: AllItem
+            },
+            {
+              path: "product/:itemId",
+              name: "SingleItem",
+              component: SingleItem
+            }
+          ]
+        },
+        {
+          path: "faq",
+          name: "Faq",
+          component: Faq
+        },
+        {
+          path: "createOrder",
+          name: "CreateOrder",
+          component: CreateOrder
+        }
       ]
-    },
+    }
   ]
-})
+});
